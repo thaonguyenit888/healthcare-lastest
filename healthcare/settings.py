0c43758e44ed47120d5b4c194d9e0ca238230b2a
thaonguyenit888 <thaonguyenit888@gmail.com>
2026-06-07 19:05:46 +0700

    Update settings.py

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -87,11 +87,16 @@ WSGI_APPLICATION = 'healthcare.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": 'Thaonguyen@1102',
+        "HOST": "db.bxyohezfqdpcbflckony.supabase.co",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
